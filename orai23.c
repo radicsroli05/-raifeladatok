3e30f4dce04dde09726799b5efefb0f9f3df7a0c
radicsroli05 <70635789+radicsroli05@users.noreply.github.com>
2026-03-23 17:11:41 +0100

    Add files via upload

--- orai23.c
+++ orai23.c
@@ -4,13 +4,15 @@
 #include <stdlib.h>
     
 int main(int argc, string argv[]){
-    int a,b,c;
  
     printf("%s, %s\n", argv[1], argv[2]);
-    a=atoi(argv[1]);
-    b=atoi(argv[2]);
-    c=a+b;
-    printf("A két szám összege: %d\n", c);
+    if (argv[1] == "Debrecen"){
+        printf("Örülök!");
+        }
+    if (strcmp(argv[1], "Debrecen")==0)
+    {
+        printf("Örülök!");
+    }
     return 0;
 }
     
