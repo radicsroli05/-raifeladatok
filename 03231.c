1a66a91cdf528e46ce2842fa8e1f59d92124863d
radicsroli05 <70635789+radicsroli05@users.noreply.github.com>
2026-03-23 16:45:44 +0100

    .3

--- 03231.c
+++ 03231.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 //#include "prog1.h"
+#define MERET 6
+
 int main(){
-    char szoveg[100];
+    char szoveg[MERET];
     printf("Adja meg a nevét: ");
-    fgets(szoveg, 100, stdin);
+    fgets(szoveg, MERET, stdin);
+    szoveg[strlen(szoveg)-1]='\0';
     printf("Szia %s!\n", szoveg);
     return 0;
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
